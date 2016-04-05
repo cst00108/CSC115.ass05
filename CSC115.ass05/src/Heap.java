cf4d2f37169a37d131e0c8e736387a1a7d04f2e2
DJ <cst00108@hotmail.com>
2016-04-04 23:20:05 -0700

    Added files via upload

--- CSC115.ass05/src/Heap.java
+++ CSC115.ass05/src/Heap.java
@@ -1,37 +1,81 @@
-import java.util.NoSuchElementException;
-
 /*
- * The shell of the class, to be completed as part
- * of CSC115 Assignment 5: Emergency Room.
+ * Name:      Jason Donald
+ * ID:        V00861539
+ * Date:      2006-04-04
+ * Filename:  Heap.java
+ * Details:   CSC115 Assignment 5
  */
+
+import java.util.NoSuchElementException;
+import java.util.Random;
 
 @SuppressWarnings({"unchecked"})
-
-/*
- * Complete this class as per the Heap.html specification document.
- * Fill in any of the parts that have the comment:
- *	/********  COMPLETE *******
- * Do not change method headers or code that has been supplied.
- * Delete all messages to you, including this one, before submitting.
- */
 
 public class Heap {
 	private Comparable[] heapArray;
     private static final int SIZE = 1000;
 	private int size = 0;
-        
+
+	/**
+	 * Create an empty heap.
+	 */
 	public Heap() {
             heapArray = new Comparable[SIZE];
 	}
         
 	private void bubbleUp(){
-
+		for(int childIndex = size-1, parentIndex = childIndex/2; 
+				childIndex != parentIndex 
+				&& heapArray[childIndex].compareTo(heapArray[parentIndex]) < 0;
+				childIndex = parentIndex, parentIndex /= 2){
+			
+			Comparable temp = heapArray[childIndex];
+			heapArray[childIndex] = heapArray[parentIndex];
+			heapArray[parentIndex] = temp;
+		}
 	}
 
 	private void bubbleDown(){
+		for(int parentIndex = 0, childToCompare = -1;
+				true;
+				parentIndex = childToCompare){
+	
+			int firstChildIndex = 2 * parentIndex + 1; 
+			int secondChildIndex = firstChildIndex + 1;
+			
+			//if just one child, skip the first compareTo().
+			//This means the first born is the last element in the array.
+			if(firstChildIndex >= this.size()){ //reached the end
+				break;
+			} else if(secondChildIndex >= this.size()){
+				childToCompare = firstChildIndex;
+			} else { //parent has two chidren
+				int result = 
+						heapArray[firstChildIndex]
+						.compareTo(heapArray[secondChildIndex]);
+				
+				childToCompare = 
+						(result < 0) ? firstChildIndex : secondChildIndex; 
+			}
+			
+			int result = 
+					heapArray[childToCompare].compareTo(heapArray[parentIndex]);
+			
+			if(result < 0){ //swap the parent and the child
+				Comparable temp = heapArray[childToCompare];
 
+				heapArray[childToCompare] = heapArray[parentIndex];
+				heapArray[parentIndex] = temp;
+			} else {
+				break;
+			}
+		}
 	}
-
+	
+	/**
+	 * Inserts an item into the heap.
+	 * @param item The newly added item.
+	 */
 	public void insert(Comparable item){
 		try{
 			heapArray[size] = item;
@@ -44,15 +88,28 @@ public class Heap {
 		bubbleUp();
 	}
 	
+	/**
+	 * @return True if the heap is empty, false if it is not.
+	 */
 	public boolean isEmpty(){
 		return size == 0;
 	}
 
+	/**
+	 * @return The number of items in the heap.
+	 */
 	public int size(){
 		return size;
 	}
 	
+	/**
+	 * Retrieves, without removing the item in the root.
+	 * @return The top item in the tree.
+	 * @throws java.util.NoSuchElementException if the heap is empty.
+	 */
 	public Comparable getRootItem(){
+		throwIfEmpty(); //throws exception if no elements
+
 		return heapArray[0];
 	}
 	
@@ -66,20 +123,181 @@ public class Heap {
 		heapArray = newHeapArray;
 	}
 
+	/**
+	 * Removes the item at the root node of the heap.
+	 * @return The item at the root of the heap.
+	 * @throws java.util.NoSuchElementException if the heap is empty.
+	 */
 	public Comparable removeRootItem(){
+		throwIfEmpty(); //throws exception if no elements
+		
 		Comparable toReturn = heapArray[0];
-//double check if decreases size AFTER the assignment
-		heapArray[0] = heapArray[size--];
+		heapArray[0] = heapArray[--size];
 		
 		bubbleDown();
 		
 		return toReturn;
 	}
-	
-	public static void main(String[] aaarg){
-		int[] ints = new int[0];
-		
-		ints[0] = -1;
+
+	private void throwIfEmpty(){
+		if(this.size() == 0){
+			throw new NoSuchElementException("No Elements in heap.");
+		}
 	}
 	
+	private void printArray(){
+		
+		for(int index=0; index<this.size(); index++){
+			System.out.println(
+					"heapArray[" + index + "] = " + heapArray[index]);
+		}
+		
+		System.out.println();
+	}
+	
+	/**
+	 * Used for internal testing purposes.
+	 * @param aaarg Not used.
+	 */
+	public static void main(String[] aaarg){
+		final Random random = new Random();
+		final String[] CATEGORY = { 
+				"Life-threatening", "Chronic", "Major fracture", "Walk-in"
+		};
+
+		class Patient extends ER_Patient{		
+
+			public Patient() {
+				super(CATEGORY[random.nextInt(CATEGORY.length)]);
+			}
+		}
+
+		class Util{
+
+			//test if all of Heaps methods which say it's empty work
+			public boolean hasNoneTest(Heap emptyHeap){				
+				if(emptyHeap.size != 0 || emptyHeap.size() != 0 
+						|| !emptyHeap.isEmpty()){
+					
+					return false;
+				}
+				
+				try{
+					emptyHeap.getRootItem();
+
+					return false;
+				} catch(NoSuchElementException x){
+					//passes
+				}
+
+				try{
+					emptyHeap.removeRootItem();
+					
+					return false;
+				} catch(NoSuchElementException x){
+					//passes
+				}
+
+				return true;
+			}
+		}
+
+		//test increaseSize()
+		Heap heap = new Heap();
+
+		for(int index=0; index<2001; index++){
+			heap.insert(index);
+		}
+		
+		if(heap.size() != 2001 && heap.heapArray.length > Heap.SIZE){
+			System.out.println("increaseSize() failed");
+		}
+		
+		//insert and check if Objects come out in the right order
+		heap = new Heap();
+		
+		for(int index=10; index>0; index--){
+			heap.insert(index);
+		}
+		
+		for(int index=1; index<=10; index++){		
+			int integer = ((Integer)heap.removeRootItem()).intValue();
+			
+			if(integer != index){
+				System.out.println("Failed heap order. Expected " + index 
+						+ " got " + integer);
+			}
+		}
+		
+		if(heap.size() != 0){
+			System.out.println("Failed heap's size() after extracting all objects");
+		}
+		
+		//check all empty tests
+		if(!new Util().hasNoneTest(heap)){
+			System.out.println("Failed test for empty heap after 1 - 10 " 
+					+ "emptied.");
+		}
+		
+		//insert 100 Patients (ER_Patients sub class)
+		for(int index=1; index<=100; index++){		
+			heap.insert(new Patient());
+		}
+		
+		try{
+			if(((ER_Patient)heap.removeRootItem()).getSymptomCategory() 
+					!= CATEGORY[0]
+					&& ((ER_Patient)heap.removeRootItem()).getSymptomCategory() 
+					!= CATEGORY[0]
+					&& ((ER_Patient)heap.removeRootItem()).getSymptomCategory() 
+					!= CATEGORY[0]){
+
+				System.out.println(
+						"Failed extracting Patients with correct priority");
+			}
+		} catch(NoSuchElementException x){
+			System.out.println("Failed removeRootItem() for Patient.");
+		}
+		
+		//try out getRootItem() when none added
+		try{
+			new Heap().getRootItem();
+			throw new Throwable();
+		} catch(NoSuchElementException x){
+			//test checks out
+		} catch(Throwable x){
+			System.out.println("Didn't catch NoSuchElementException "
+					+ "in getRootItem().");
+		}
+
+		//try out removeRootItem() when none added
+		try{
+			new Heap().removeRootItem();
+			throw new Throwable();
+		} catch(NoSuchElementException x){
+			//test checks out
+		} catch(Throwable x){
+			System.out.println("Didn't catch NoSuchElementException "
+					+ "in getRootItem().");
+		}
+		
+		//insert and delete single object in new heap and see its empty
+		heap = new Heap();
+		
+		if(!new Util().hasNoneTest(heap)){
+			System.out.println("Failed test for empty heap before inserted.");
+		}
+
+		try{
+			heap.insert(123);
+			heap.removeRootItem();
+		} catch(NoSuchElementException x){
+			System.out.println(
+					"Failed removal after inserting single element.");
+		}
+		
+		if(!new Util().hasNoneTest(heap)){
+			System.out.println("Failed test for empty heap after emptied.");
+		}
+	}	
 }
--- CSC115.ass05/src/PriorityQueue.java
+++ CSC115.ass05/src/PriorityQueue.java
@@ -1,27 +1,137 @@
+/*
+ * Name:      Jason Donald
+ * ID:        V00861539
+ * Date:      2006-04-04
+ * Filename:  PriorityQueue.java
+ * Details:   CSC115 Assignment 5
+ */
+
 import java.util.NoSuchElementException;
-
-/*
- * The shell of the class, to be completed as part of the
- * CSC115 Assignment 5 : Emergency Room
- */
-
-/*
- * Complete this class as per the Heap.html specification document.
- * Fill in any of the parts that have the comment:
- *	/********  COMPLETE *******/
- * Do not change method headers or code that has been supplied.
- * Delete all messages to you, including this one, before submitting.
- */
+import java.util.Random;
 
 public class PriorityQueue {
 	
 	private Heap heap;
 
+	/**
+	 * Creates an empty priority queue.
+	 */
 	public PriorityQueue() {
- 	/********  COMPLETE *******/
+		heap = new Heap();
 	}
 
- 	/********  COMPLETE *******/
+	/**
+	 * Inserts an item into the queue.
+	 * @param item The item to insert.
+	 */
+	public void enqueue(Comparable item){
+		heap.insert(item);
+	}
+	
+	/**
+	 * Removes the highest priority item from the queue.
+	 * @return The item.
+	 * @throws java.util.NoSuchElementException if the queue is empty.
+	 */
+	public Comparable dequeue(){
+		return heap.removeRootItem();
+	}
+	
+	/**
+	 * Retrieves, but does not remove the next item out of the queue.
+	 * @return The item with the highest priority in the queue.
+	 * @throws java.util.NoSuchElementException if the queue is empty.
+	 */
+	public Comparable peek(){
+		return heap.getRootItem();
+	}
+	
+	/**
+	 * @return True if the queue is empty, false if it is not.
+	 */
+	public boolean isEmpty(){
+		return heap.isEmpty();
+	}
+	
+	/**
+	 * Used for internal testing purposes.
+	 * @param aaarg Not used.
+	 */
+	public static void main(String[] aaarg){
+		final Random random = new Random();
+		final String[] CATEGORY = { 
+				"Life-threatening", "Chronic", "Major fracture", "Walk-in"
+		};
+
+		class Patient extends ER_Patient{		
+
+			public Patient() {
+				super(CATEGORY[random.nextInt(CATEGORY.length)]);
+			}
+		}
+		
+		try{
+			new PriorityQueue().peek();
+			System.out.println("Error - peeked with no elements");
+		} catch(NoSuchElementException x){
+			//
+		} catch(Throwable x){
+			System.out.println("Error - peeked with no elements");
+		}
+		
+		//test increaseSize()
+		PriorityQueue heap = new PriorityQueue();
+
+		for(int index=0; index<2001; index++){
+			heap.enqueue(index);
+		}
+		
+		//insert and check if Objects come out in the right order
+		heap = new PriorityQueue();
+		
+		for(int index=10; index>0; index--){
+			heap.enqueue(index);
+		}
+		
+		for(int index=1; index<=10; index++){		
+			int integer = ((Integer)heap.dequeue()).intValue();
 			
+			if(integer != index){
+				System.out.println("Failed heap order. Expected " + index 
+						+ " got " + integer);
+			}
+		}
+		
+		//insert 100 Patients (ER_Patients sub class)
+		for(int index=1; index<=100; index++){		
+			heap.enqueue(new Patient());
+		}
+		
+		try{
+			if(((ER_Patient)heap.dequeue()).getSymptomCategory() 
+					!= CATEGORY[0]
+					&& ((ER_Patient)heap.dequeue()).getSymptomCategory() 
+					!= CATEGORY[0]
+					&& ((ER_Patient)heap.dequeue()).getSymptomCategory() 
+					!= CATEGORY[0]){
+
+				System.out.println(
+						"Failed extracting Patients with correct priority");
+			}
+		} catch(NoSuchElementException x){
+			System.out.println("Failed dequeue() for Patient.");
+		}
+		
+		//try out dequeue() when none added
+		try{
+			new PriorityQueue().dequeue();
+			throw new Throwable();
+		} catch(NoSuchElementException x){
+			//test checks out
+		} catch(Throwable x){
+			System.out.println("Didn't catch NoSuchElementException "
+					+ "in dequeue().");
+		}
+	}
 }
 	
